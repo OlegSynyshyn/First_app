6d742b2cad39f967ce87183be66174d616a137ff
Oleh <oleg.synyshyn10@gmail.com>
2024-06-20 18:17:25 +0300

    123

--- main.py
+++ main.py
@@ -4,15 +4,28 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.textinput import TextInput
 # Екран (об'єкт класу Screen) – це віджет типу "макет" (Screen – спадкоємець класу RelativeLayout).
 # ScreenManager - це особливий віджет, який робить видимим один із прописаних у ньому екранів.
 
 class FirstScr(Screen):
     def __init__(self, name='first'):
         super().__init__(name=name) # ім'я екрана має передаватися конструктору класу Screen
-        btn = Button(text="Перейти на інший екран")
+        text = Label(text="Виберіть дію")
+
+        btn = Button(text="Долар в грн")
+        btn2 = Button(text="Грн в долар")
+
+        layout = BoxLayout(orientation= 'vertical', spacing="20sp")
+        layout.add_widget(text)
+        layout.add_widget(btn)
+        layout.add_widget(btn2)
+
+
         btn.on_press = self.next
-        self.add_widget(btn) # екран - це віджет, на якому можуть створюватись всі інші (нащадки)
+        self.add_widget(layout) # екран - це віджет, на якому можуть створюватись всі інші (нащадки)
+
+
     def next(self):
         self.manager.transition.direction = 'left' # об'єкт класу Screen має властивість manager 
         # - це посилання на батьківський клас
@@ -21,9 +34,21 @@ class FirstScr(Screen):
 class SecondScr(Screen):
     def __init__(self, name='second'):
         super().__init__(name=name)
-        btn = Button(text="Повернися, повернися!")
+
+
+        textinput = TextInput(text='Hello world', multiline=False)
+
+        text = Label(text="Результат:")
+
+
+        btn = Button(text="Назад")
+
+        layout = BoxLayout(orientation= 'vertical', spacing="20sp")
+        layout.add_widget(text)
+        layout.add_widget(textinput)
+        layout.add_widget(btn)
         btn.on_press = self.next
-        self.add_widget(btn)
+        self.add_widget(layout)
         
     def next(self):
         self.manager.transition.direction = 'right'
